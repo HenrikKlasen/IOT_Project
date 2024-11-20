78282fb82aa1c1eddc4b5826c5879cc175054a0f
HenrikKlasen <henrik.klasen.001@student.uni.lu>
2024-11-20 19:45:02 +0100

    [Comm/Arduino] Establish first communication protocol over serial with pyserial input parsing

---
+++ ArduinoFirmware/serial_reader.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+
+def readserial(comport, baudrate, timestamp=False):
+
+    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+
+    while True:
+
+        temp = ser.readline().decode().strip()
+        humidity = ser.readline().decode().strip()
+
+        if temp and humidity and timestamp:
+            timestamp = time.strftime('%H:%M:%S')
+            print(f'{timestamp} > Temp: {temp} > Humidity: {humidity}')
+        elif temp and humidity:
+            print(temp, humidity)
+
+
+if __name__ == '__main__':
+    readserial('COM3', 9600, True)
