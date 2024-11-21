566ff2016e235b1dff1e7c167280856f51d33756
HenrikKlasen <henrik.klasen.001@student.uni.lu>
2024-11-21 10:57:00 +0100

    [COAP-Server] Coap-Server first version working

--- ArduinoFirmware/serial_reader.py
+++ ArduinoFirmware/serial_reader.py
@@ -1,7 +1,9 @@
 import serial
 import time
 import serial_parser
-
+from aiocoap import *
+import asyncio
+import json
 
 def readserial(comport, baudrate, timestamp=False):
 
@@ -12,10 +14,40 @@ def readserial(comport, baudrate, timestamp=False):
         temp = ser.readline().decode().strip()
         humidity = ser.readline().decode().strip()
 
-        if temp and humidity and timestamp:
-            timestamp = time.strftime('%H:%M:%S')
-            print(serial_parser.parse_to_json(timestamp=timestamp, roomID=1, temp=temp, humidity=humidity))
+        timestamp = time.strftime('%Y/%m/%d|%H:%M:%S')
+        asyncio.run(
+            post_sensor_data(serial_parser.parse_to_json(timestamp=timestamp, roomID=1, temp=temp, humidity=humidity).encode("utf-8"))
+            )
+        asyncio.run(
+            get_sensor_data()
+        )
 
+async def get_sensor_data():
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri='coap://127.0.0.1:5683/Room1')
+
+    try:
+        response = await protocol.request(request).response
+        print('Result: %s\n%r' % (response.code, response.payload))
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+
+async def post_sensor_data(data):
+    protocol = await Context.create_client_context()
+
+    request = Message(code=POST, uri='coap://127.0.0.1:5683/Room1', payload=data)
+
+    try:
+        response = await protocol.request(request).response
+        print('Result: %s\n%r' % (response.code, response.payload))
+    except Exception as e:
+        print('Failed to post resource:')
+        print(e)
 
 if __name__ == '__main__':
+    #print(asyncio.run(get_sensor_data()))
     readserial('COM3', 9600, timestamp=True)
+
+
